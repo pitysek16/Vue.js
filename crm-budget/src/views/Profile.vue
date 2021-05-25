645aa22a010bb809ff2f4edf7fea2dafb0ec4666
Katerina Pidan <pitysek@bk.ru>
2021-05-25 15:49:40 +0300

    add: fn for change name in Profile

--- crm-budget/src/views/Profile.vue
+++ crm-budget/src/views/Profile.vue
@@ -7,18 +7,66 @@
     <form class="form">
       <div class="input-field">
         <input
-            id="description"
-            type="text"
+          id="description"
+          type="text"
+          v-model.trim="name"
+          :class="{
+            invalid: $v.name.$dirty && !$v.name.required,
+          }"
+        />
+        <small
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+          >Введите ваше имя</small
         >
-        <label for="description">Имя</label>
-        <span
-              class="helper-text invalid">name</span>
       </div>
 
-      <button class="btn waves-effect waves-light" type="submit">
+      <button
+        class="btn waves-effect waves-light"
+        type="submit"
+        @click.prevent="submitUpdateName"
+      >
         Обновить
         <i class="material-icons right">send</i>
       </button>
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import { required } from 'vuelidate/lib/validators';
+export default {
+  name: 'Profile',
+  data() {
+    return {
+      name: '',
+    };
+  },
+  computed: {
+    ...mapGetters(['info']),
+  },
+  mounted() {
+    this.name = this.info.name;
+  },
+  validations: {
+    name: { required },
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitUpdateName() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      try {
+        await this.updateInfo({
+          name: this.name,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
